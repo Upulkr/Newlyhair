87de1d7ac789d0a0cbfc4c329618f7ca12b956af
Upul <upulkr4@gmail.com>
2023-10-20 21:46:40 +0530

    team card edited

--- src/widgets/cards/team-card.jsx
+++ src/widgets/cards/team-card.jsx
@@ -11,7 +11,7 @@ export function TeamCard({ img, name, position, socials, id }) {
 // //  ()=> setActiveCard(activeCard)
 // }
   return(
-    <Card className=" sm:flex-col-1 md:flex-cols-2 lg:flex-cols-3 xl:flex-cols-4 2xl:flex-cols-6  w-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..." color="yellow" variant="gradient" >
+    <Card className=" sm:flex-col-1 md:flex-cols-2 lg:flex-cols-3 xl:flex-cols-4 2xl:flex-cols-6  w-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..." color="" variant="gradient" >
     <CardHeader floated={false} className=" rounded object-cover object-center">
       
     </CardHeader>
