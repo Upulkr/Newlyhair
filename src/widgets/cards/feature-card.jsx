e068781365f509063df0df979549ec286f614463
Upul <upulkr4@gmail.com>
2023-10-30 23:32:29 +0530

    header and service page edited

--- src/widgets/cards/feature-card.jsx
+++ src/widgets/cards/feature-card.jsx
@@ -4,29 +4,41 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export function FeatureCard({ img, title, description }) {
   return (
-<div >
+  
 
-  <div className=" bg-white dark:bg-black" >
- 
-    <LazyLoadImage 
-      className="rounded-lg w-full"
-      src={img}
-      alt="services"
-    />
-
-    {/* <p className="text-indigo-500 font-semibold text-base mt-2"></p>
-  */}
-    <h1 className=" dark:text-white  font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate">
-    {title}
-    </h1>
- 
-    <div className="max-w-full">
-      <p className="text-base font-medium tracking-wide text-gray-600 mt-1  dark:text-white">
-      {description}
-      </p>
+    <div className=" bg-gray-50 overflow-hidden">
+  <div className="m-auto px-6 space-y-8 text-gray-500 md:px-12">
+    <div>
+     
+      <h2 className="mt-4 text-2xl text-gray-900 font-bold md:text-4xl text-center">
+     {title}
+      
+      </h2>
     </div>
+    <>
+      <>
+        <>
+          <img
+            src={img}
+            className="w-max"
+            width={512}
+            height={512}
+            alt="burger illustration"
+          />
+          <div className="space-y-2">
+            
+            <p className="text-sm text-gray-600">
+              {description}
+            </p>
+          </div>
+   
+        </>
+      </>
+    </>
   </div>
 </div>
+
+  
 
 
     
@@ -40,3 +52,29 @@ export function FeatureCard({ img, title, description }) {
 FeatureCard.displayName = "/src/widgets/layout/feature-card.jsx";
 
 export default FeatureCard;
+
+
+
+      
+{/* <div >
+
+  <div className=" bg-white dark:bg-black" >
+ 
+    <LazyLoadImage 
+      className="rounded-lg w-full"
+      src={img}
+      alt="services"
+    />
+
+    
+    <h1 className=" dark:text-white  font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate">
+    {title}
+    </h1>
+ 
+    <div className="max-w-full">
+      <p className="text-base font-medium tracking-wide text-gray-600 mt-1  dark:text-white">
+      {description}
+      </p>
+    </div>
+  </div>
+</div> */}
