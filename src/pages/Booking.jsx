dcd0aeb16f041eb79730957d65678dc77b27f47e
Upul <upulkr4@gmail.com>
2023-10-31 10:15:42 +0530

    Booking details to email

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import "@dotlottie/player-component";
-import FooterTwo from './FooterTwo';
+import FooterTwo from "./FooterTwo";
+import emailJs from "@emailjs/browser";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 function Booking() {
+  const form = useRef();
+  const serviceId = process.env.CONTACT_SERVICE_ID2;
+  const templateId = process.env.CONTACT_TEMPLATE_ID2;
+  const publicKey = process.env.CONTACT_PUBLIC_KEY2;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+   
+    emailJs.sendForm(serviceId, templateId, e.target, publicKey).then(
+      (result) => {
+        console.log(result.text);
+        toast.success("Well done");
+      },
+      (error) => {
+        console.log(error.text);
+        toast.error("not submitted");
+      }
+    );
+  };
   return (
     <>
       <div className="grid-row-2  grid bg-white dark:bg-black  ">
@@ -25,13 +49,17 @@ function Booking() {
             />
           </div>{" "}
           <div className="flex items-center justify-center">
-            <form className=" w-2/3 justify-center ">
+            <form
+              className=" w-2/3 justify-center "
+              ref={form}
+              onSubmit={sendEmail}
+            >
               {" "}
               <div className="">
                 {" "}
                 <label
                   htmlFor="name"
-                  className="  dark:text-white m-3 block text-base font-medium text-[#07074D]"
+                  className="  m-3 block text-base font-medium text-[#07074D] dark:text-white"
                 >
                   Full Name
                 </label>
@@ -40,13 +68,13 @@ function Booking() {
                   name="name"
                   id="name"
                   placeholder="Full Name"
-                  className="w-full rounded-md border border-blue-gray-900 bg-white px-6 p-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-blue-gray-900 bg-white p-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
               <div className="mb-5">
                 <label
                   htmlFor="phone"
-                  className="  dark:text-white mb-3 block text-base font-medium text-[#07074D]"
+                  className="  mb-3 block text-base font-medium text-[#07074D] dark:text-white"
                 >
                   Phone Number
                 </label>
@@ -55,13 +83,13 @@ function Booking() {
                   name="phone"
                   id="phone"
                   placeholder="Enter your phone number"
-                  className="w-full rounded-md border border-blue-gray-900 bg-white px-6 p-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-blue-gray-900 bg-white p-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
               <div className="mb-5">
                 <label
                   htmlFor="email"
-                  className="  dark:text-white mb-3 block text-base font-medium text-[#07074D]"
+                  className="  mb-3 block text-base font-medium text-[#07074D] dark:text-white"
                 >
                   Email Address
                 </label>
@@ -70,7 +98,7 @@ function Booking() {
                   name="email"
                   id="email"
                   placeholder="Enter your email"
-                  className="w-full rounded-md border border-blue-gray-900 bg-white px-6 p-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  className="w-full rounded-md border border-blue-gray-900 bg-white p-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
               <div className="-mx-3 flex flex-wrap">
@@ -78,7 +106,7 @@ function Booking() {
                   <div className="mb-5">
                     <label
                       htmlFor="date"
-                      className="  dark:text-white mb-3 block text-base font-medium text-[#07074D]"
+                      className="  mb-3 block text-base font-medium text-[#07074D] dark:text-white"
                     >
                       Date
                     </label>
@@ -86,7 +114,7 @@ function Booking() {
                       type="date"
                       name="date"
                       id="date"
-                      className="w-full rounded-md border border-blue-gray-900 bg-white px-6 p-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                      className="w-full rounded-md border border-blue-gray-900 bg-white p-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                     />
                   </div>
                 </div>
@@ -102,13 +130,13 @@ function Booking() {
                       type="time"
                       name="time"
                       id="time"
-                      className="w-full rounded-md border border-blue-gray-900 bg-white px-6 p-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                      className="w-full rounded-md border border-blue-gray-900 bg-white p-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                     />
                   </div>
                 </div>
               </div>
               <div>
-                <button className="  dark:text-white hover:shadow-form w-full rounded-md bg-[#6A64F1] px-8 p-2 text-center text-base font-semibold text-white outline-none">
+                <button type='submi'className= "  hover:shadow-form w-full rounded-md bg-[#6A64F1] p-2 px-8 text-center text-base font-semibold text-white outline-none dark:text-white">
                   Book Appointment
                 </button>
               </div>
@@ -118,8 +146,6 @@ function Booking() {
       </div>
       <FooterTwo />
     </>
-
-   
   );
 }
 
